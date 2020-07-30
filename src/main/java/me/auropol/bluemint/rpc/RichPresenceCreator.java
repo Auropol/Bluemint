a9a2cbeb894a6b8cbd686bbd97f7b53e379e542d
Auropol <62548533+Auropol@users.noreply.github.com>
2020-07-30 16:07:33 +0200

    Fixed NullPointerException causing methods

--- src/main/java/me/auropol/bluemint/rpc/RichPresenceCreator.java
+++ src/main/java/me/auropol/bluemint/rpc/RichPresenceCreator.java
@@ -13,10 +13,14 @@ import java.util.concurrent.TimeUnit;
 
 public class RichPresenceCreator {
     private static DiscordRPC rpc;
+    static {
+        rpc = DiscordRPC.INSTANCE;
+        presence = new DiscordRichPresence();
+    }
     private static DiscordRichPresence presence;
-    public void createSimpleRichPresence(long appID, String state, String details, String imageDesc, String imagePath)  {
+    public void createSimpleRichPresence(String appID, String state, String details, String imageDesc, String imagePath)  {
         DiscordEventHandlers handlers = new DiscordEventHandlers();
-        rpc.Discord_Initialize(String.valueOf(appID), handlers, true, "");
+        rpc.Discord_Initialize(appID, handlers, true, "");
         presence.details = details;
         presence.state = state;
         presence.largeImageKey = imagePath;
@@ -31,51 +35,51 @@ public class RichPresenceCreator {
             exc.printStackTrace();
         }
     }
-      public void createRichPresenceFromArrays(long appID, String[] states, String[] details, String[] imageDescriptions, String[] imagePaths, boolean constantlyUpdateAsGif, boolean updateImagesRandomly) {
-          String detail = RandStream.pickFrom(details);
-          String state = RandStream.pickFrom(states);
-          String imageText = RandStream.pickFrom(imageDescriptions);
-          DiscordEventHandlers handlers = new DiscordEventHandlers();
-          final Thread thread = new Thread() {
-              @Override
-              public String name() {
-                  return "BLUEMINT-RICHPRESENCE";
-              }
-          };
-          rpc.Discord_Initialize(String.valueOf(appID), handlers, true, "");
-          presence.details = detail;
-          presence.state = state;
-          presence.largeImageKey = RandStream.pickFrom(imagePaths);
-          presence.largeImageText = imageText;
-          if(constantlyUpdateAsGif) {
-              Logger.manage().info("Gif enabled", thread);
-              Runnable runnable = new Runnable() {
-                  @Override
-                  public void run() {
-                      rpc.Discord_UpdatePresence(presence);
-                  }
-              };
-              if(new Random().nextBoolean()) {
-                  rpc.Discord_UpdatePresence(presence);
-              }
-          try {
-              if(updateImagesRandomly) {
-              presence.details = detail;
-              presence.state = state;
-              presence.largeImageKey = RandStream.pickFrom(imagePaths);
-              presence.largeImageText = imageText;
-              } else  {
-                  presence.details = detail;
-                  presence.state = state;
-                  presence.largeImageKey = RandStream.pickGradientlyFrom(imagePaths);
-                  presence.largeImageText = imageText;
-              }
+    public void createRichPresenceFromArrays(String appID, String[] states, String[] details, String[] imageDescriptions, String[] imagePaths, boolean constantlyUpdateAsGif, boolean updateImagesRandomly) {
+        String detail = RandStream.pickFrom(details);
+        String state = RandStream.pickFrom(states);
+        String imageText = RandStream.pickFrom(imageDescriptions);
+        final DiscordEventHandlers handlers = new DiscordEventHandlers();
+        final Thread thread = new Thread() {
+            @Override
+            public String name() {
+                return "BLUEMINT-RICHPRESENCE";
+            }
+        };
+        rpc.Discord_Initialize(appID, handlers, true, "");
+        presence.details = detail;
+        presence.state = state;
+        presence.largeImageKey = RandStream.pickFrom(imagePaths);
+        presence.largeImageText = imageText;
+        if(constantlyUpdateAsGif) {
+            Logger.manage().info("Gif enabled", thread);
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    rpc.Discord_UpdatePresence(presence);
+                }
+            };
+            if(new Random().nextBoolean()) {
+                rpc.Discord_UpdatePresence(presence);
+            }
+            try {
+                if(updateImagesRandomly) {
+                    presence.details = detail;
+                    presence.state = state;
+                    presence.largeImageKey = RandStream.pickFrom(imagePaths);
+                    presence.largeImageText = imageText;
+                } else  {
+                    presence.details = detail;
+                    presence.state = state;
+                    presence.largeImageKey = RandStream.pickGradientlyFrom(imagePaths);
+                    presence.largeImageText = imageText;
+                }
 
-          } catch(Exception exc) {
-              exc.printStackTrace();
-          }
-          } else {
-              Logger.manage().info("Gif disabled", thread);
-          }
-      }
+            } catch(Exception exc) {
+                exc.printStackTrace();
+            }
+        } else {
+            Logger.manage().info("Gif disabled", thread);
+        }
+    }
 }
