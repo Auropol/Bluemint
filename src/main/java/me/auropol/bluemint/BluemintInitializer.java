8218b72ddf6f9be57b3a1ff6c2fd974a0e673ebe
Auropol <62548533+Auropol@users.noreply.github.com>
2020-08-01 16:43:00 +0200

    Patched the date system, patched cause() method

--- src/main/java/me/auropol/bluemint/BluemintInitializer.java
+++ src/main/java/me/auropol/bluemint/BluemintInitializer.java
@@ -1,45 +1,38 @@
 package me.auropol.bluemint;
 import me.auropol.bluemint.primitive.Container;
+import me.auropol.bluemint.primitive.Wrapper;
 import me.auropol.bluemint.util.Thread;
 import me.auropol.bluemint.util.Logger;
 import me.auropol.bluemint.util.Map;
-import sun.util.calendar.BaseCalendar;
 
+import javax.xml.datatype.Duration;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.namespace.QName;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Date;
-import java.util.Random;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class BluemintInitializer {
     public static String VERSION = "0.1-SNAPSHOT";
     public static String NAME = "Bluemint Library";
     public static String CREDITS = "Auropol";
-    public static BaseCalendar.Date date;
-    static {
-        date = new BaseCalendar.Date() {
-            @Override
-            public int getNormalizedYear() {
-                return 2020;
-            }
-
-            @Override
-            public void setNormalizedYear(int i) {
-             i = getNormalizedYear();
-            }
-        };
-    }
+    public static LocalDateTime date = LocalDateTime.now();
     private static String currentDate() {
-        int minutes = date.getMinutes();
-        int hours = date.getHours();
+        int minutes = date.getMinute();
+        int hours = date.getHour();
         int days = date.getDayOfMonth();
-        int months = date.getMonth();
+        int months = date.getMonth().getValue();
         int years = date.getYear();
         return Integer.toString(minutes) + hours + days + months + years;
     }
 
-    public void main(String[] args) {
+    public void init() {
         StringWriter strWriter = new StringWriter();
         Thread thread = new Thread() {
 
@@ -60,9 +53,7 @@ public class BluemintInitializer {
         }
     }
     public String cause() {
-        int minValue = 1;
-        int maxValue = 6;
-        int nextCause = new Random().nextInt(maxValue - minValue + 1) - minValue;
+        int nextCause = Wrapper.wrap().pickRandomlyFromRange(1, 6);
         String cause1 = Map.assignToFrom("I will fix it soon !", 1, nextCause);
         String cause2 = Map.assignToFrom("I feel sorry for you", 2, nextCause);
         String cause3 = Map.assignToFrom("Blew my processing unit !", 3, nextCause);
