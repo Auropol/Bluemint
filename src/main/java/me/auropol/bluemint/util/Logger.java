f4bace6dce7cd5ba578dc4e6c0f6205efd5e4c5c
Auropol <62548533+Auropol@users.noreply.github.com>
2020-08-04 17:28:46 +0200

    Added log() method to Logger

--- src/main/java/me/auropol/bluemint/util/Logger.java
+++ src/main/java/me/auropol/bluemint/util/Logger.java
@@ -1,23 +1,68 @@
 package me.auropol.bluemint.util;
+import me.auropol.bluemint.primitive.Wrappers;
 import me.auropol.bluemint.util.Thread.ThreadUtil;
+
 public class Logger {
+    public static LoggerHelper log;
+    private Wrappers wrappers = new Wrappers() {
+        @Override
+        public Object wrapper() {
+            return null;
+        }
+    };
+
     public static Logger manage() {
         return new Logger();
     }
     public void info(String args, Thread thread){
-        System.out.println(ThreadUtil.generateNewThread(thread) + "(INFO/" + args + ")");
+        final String str = wrappers.catchVoid(Wrappers.VoidType.SYSTEM_PRINTLN, "",ThreadUtil.generateNewThread(thread) + "(INFO/" + args + ")");
+        log = new LoggerHelper() {
+            @Override
+            public String getLogger() {
+               return str;
+        }
+        };
+        System.out.println(str);
     }
     public void warn(String args, Thread thread){
         String yellow = "\033[0;93m";
-        System.out.println(ThreadUtil.generateNewThread(thread) + "(WARN/" + args + yellow + ")");
+        final String str = wrappers.catchVoid(Wrappers.VoidType.SYSTEM_PRINTLN, "",ThreadUtil.generateNewThread(thread) + "(WARN/" + args + yellow + ")");
+        log = new LoggerHelper() {
+            @Override
+            public String getLogger() {
+                return str;
+            }
+        };
+        System.out.println(str);
     }
     public void critical(String args, Thread thread){
         String red = "\033[0;91m";
-        System.out.println(ThreadUtil.generateNewThread(thread) + "(CRITICAL/" + args + red + ")");
+        final String str = wrappers.catchVoid(Wrappers.VoidType.SYSTEM_PRINTLN, "",ThreadUtil.generateNewThread(thread) + "(CRITICAL/" + args + red + ")");
+        log = new LoggerHelper() {
+            @Override
+            public String getLogger() {
+                return str;
+            }
+        };
+        System.out.println(str);
     }
     public void debug(String args, Thread thread) {
         String cyan = "\033[0;96m";
-        System.out.println(ThreadUtil.generateNewThread(thread) + "(DEBUG/" + args + cyan + ")");
+        final String str = wrappers.catchVoid(Wrappers.VoidType.SYSTEM_PRINTLN, "", ThreadUtil.generateNewThread(thread) + "(DEBUG/" + args + cyan + ")");
+        log = new LoggerHelper() {
+            @Override
+            public String getLogger() {
+                return str;
+            }
+        };
+        System.out.println(str);
+
+    }
+    public void log() {
+        wrappers.createNewTextFile("log.txt", log.getLogger());
+    }
+    private static abstract class LoggerHelper {
+        public abstract String getLogger();
     }
 
 }
