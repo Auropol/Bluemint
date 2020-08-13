c7236681dc6dbe67e219a80a1be86b86ef74462a
Auropol <62548533+Auropol@users.noreply.github.com>
2020-08-13 19:32:03 +0200

    Restructurized the Networker class

--- src/main/java/me/auropol/bluemint/primitive/Networker.java
+++ src/main/java/me/auropol/bluemint/primitive/Networker.java
@@ -8,14 +8,28 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 
 public interface Networker {
-     static void downloadFileFrom(String url, String name) {
+    FileChannel getChannel();
+    long getPort();
+     static Networker downloadFileFrom(String url, String name) {
          try(FileOutputStream stream = new FileOutputStream(name)) {
              ReadableByteChannel readableByteChannel = Channels.newChannel(new URL(url).openStream());
              FileChannel fileChannel = stream.getChannel();
-             stream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+             long port = stream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+             return new Networker() {
+                 @Override
+                 public FileChannel getChannel() {
+                     return fileChannel;
+                 }
+
+                 @Override
+                 public long getPort() {
+                     return port;
+                 }
+             };
 
          } catch (IOException e) {
              e.printStackTrace();
          }
+         throw new NullPointerException();
      }
 }
