e3e75cdb734c4a90aa032094e9e179b35e912847
Auropol <62548533+Auropol@users.noreply.github.com>
2020-08-14 18:36:20 +0200

    Restructrurized BigVector class
    changed the vectors from supporting primitive values with 32 or 64 bit precision to BigInteger and BigDecimal values with 32bit max value pow 2 precision

--- src/main/java/me/auropol/bluemint/graphics/BigVector.java
+++ src/main/java/me/auropol/bluemint/graphics/BigVector.java
@@ -2,284 +2,297 @@ package me.auropol.bluemint.graphics;
 
 import me.auropol.bluemint.primitive.Container;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class BigVector {
  private static abstract class BigVectorHelper2d {
-     public abstract float[] getBigVec2();
-     public abstract float getDistanceBigVec2();
+     public abstract BigDecimal[] getBigVec2();
+     public abstract BigDecimal getDistanceBigVec2();
  }
  private static BigVectorHelper2d bigVector2d;
 
- public BigVector(BigInteger x, BigInteger y, BigPoint direction) {
-     float[] point = Container.manage().createArrayFloat(x.floatValue(), y.floatValue());
-     float[] secondPoint = direction.getBigPoint();
+ public BigVector(BigDecimal x, BigDecimal y, BigPoint direction) {
+     BigDecimal[] point = new Container<BigDecimal>().createArray(x, y);
+     BigDecimal[] secondPoint = direction.getBigPoint();
      bigVector2d = new BigVectorHelper2d() {
          @Override
-         public float[] getBigVec2() {
-             return Container.manage().createArrayFloat(x.floatValue(), y.floatValue());
+         public BigDecimal[] getBigVec2() {
+             return new Container<BigDecimal>().createArray(x, y);
          }
 
          @Override
-         public float getDistanceBigVec2() {
+         public BigDecimal getDistanceBigVec2() {
              return getDir(point, secondPoint);
          }
      };
  }
-    private float getDir(float[] point, float[] secondPoint) {
-        float start = 0;
+    private BigDecimal getDir(BigDecimal[] point, BigDecimal[] secondPoint) {
+        BigDecimal start = BigDecimal.ZERO;
         for(int i = 0; i < point.length; ++i) {
-            start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+            start = start.add(point[i].abs()).add(secondPoint[i].abs());
         }
-        return (float)Math.sqrt(start);
+        return sqrt(start);
     }
-    public float[] getBigVector2d() {
+    private static BigDecimal sqrt(BigDecimal decimal) {
+     BigDecimal start = BigDecimal.ZERO;
+     BigDecimal dec = BigDecimal.valueOf(decimal.doubleValue());
+     while(!dec.equals(start)) {
+        start = dec;
+        dec = decimal.divide(start, decimal.scale(), RoundingMode.FLOOR);
+        dec = dec.add(start);
+        dec = dec.divide(BigDecimal.valueOf(2) , decimal.scale(), RoundingMode.HALF_UP);
+     }
+     return dec;
+    }
+    public BigDecimal[] getBigVector2d() {
      return bigVector2d.getBigVec2();
     }
-    public float getDirection2d() {
+    public BigDecimal getDirection2d() {
      return bigVector2d.getDistanceBigVec2();
     }
     private static abstract class BigVectorHelper3d {
-     public abstract float[] getBigVec3();
-     public abstract float getDistanceBigVec3();
+     public abstract BigDecimal[] getBigVec3();
+     public abstract BigDecimal getDistanceBigVec3();
     }
 
     public static class BigVector3D {
      private static BigVectorHelper3d bigVector3d;
-     public BigVector3D(BigInteger x, BigInteger y, BigInteger z, BigPoint.BigPoint3D direction) {
-         float[] point = Container.manage().createArrayFloat(x.floatValue(), y.floatValue(), z.floatValue());
-         float[] secondPoint = direction.getBigPoint();
+     public BigVector3D(BigDecimal x, BigDecimal y, BigDecimal z, BigPoint.BigPoint3D direction) {
+         BigDecimal[] point = new Container<BigDecimal>().createArray(x, y, z);
+         BigDecimal[] secondPoint = direction.getBigPoint();
          bigVector3d = new BigVectorHelper3d() {
              @Override
-             public float[] getBigVec3() {
-                 return Container.manage().createArrayFloat(x.floatValue(), y.floatValue(), z.floatValue());
+             public BigDecimal[] getBigVec3() {
+                 return new Container<BigDecimal>().createArray(x, y, z);
              }
 
              @Override
-             public float getDistanceBigVec3() {
+             public BigDecimal getDistanceBigVec3() {
                  return getDir(point, secondPoint);
              }
          };
      }
-        private float getDir(float[] point, float[] secondPoint) {
-            float start = 0;
+        private BigDecimal getDir(BigDecimal[] point, BigDecimal[] secondPoint) {
+            BigDecimal start = BigDecimal.ZERO;
             for(int i = 0; i < point.length; ++i) {
-                start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                start = start.add(point[i].abs().add(secondPoint[i].abs()));
             }
-            return (float)Math.sqrt(start);
+            return sqrt(start);
         }
-        public float[] getBigVector3d() {
+        public BigDecimal[] getBigVector3d() {
             return bigVector3d.getBigVec3();
         }
-        public float getDirection3d() {
+        public BigDecimal getDirection3d() {
             return bigVector3d.getDistanceBigVec3();
         }
     }
     private static abstract class BigVectorHelper4d {
-     public abstract float[] getBigVec4();
-     public abstract float getDistanceBigVec4();
+     public abstract BigDecimal[] getBigVec4();
+     public abstract BigDecimal getDistanceBigVec4();
     }
     private static abstract class BigVectorHelperInfiniteD {
-        public abstract float[] getBigVecID();
-        public abstract float getDistanceBigVecID();
+        public abstract BigDecimal[] getBigVecID();
+        public abstract BigDecimal getDistanceBigVecID();
     }
     private static abstract class DoubleBigVectorHelper2d{
-        public abstract double[] getDoubleBigVec2();
-        public abstract double getDistanceDoubleBigVec2();
+        public abstract BigInteger[] getDoubleBigVec2();
+        public abstract BigInteger getDistanceDoubleBigVec2();
     }
     private static abstract class DoubleBigVectorHelper3d {
-        public abstract double[] getDoubleBigVec3();
-        public abstract double getDistanceDoubleBigVec3();
+        public abstract BigInteger[] getDoubleBigVec3();
+        public abstract BigInteger getDistanceDoubleBigVec3();
     }
     private static abstract class DoubleBigVectorHelper4d {
-        public abstract double[] getDoubleBigVec4();
-        public abstract double getDistanceDoubleBigVec4();
+        public abstract BigInteger[] getDoubleBigVec4();
+        public abstract BigInteger getDistanceDoubleBigVec4();
     }
     private static abstract class DoubleBigVectorHelperInfiniteD{
-        public abstract double[] getDoubleBigVecID();
-        public abstract double getDistanceDoubleBigVecID();
+        public abstract BigInteger[] getDoubleBigVecID();
+        public abstract BigInteger getDistanceDoubleBigVecID();
     }
     public static class BigVector4D {
      private static BigVectorHelper4d bigVector4d;
-     public BigVector4D(BigInteger x, BigInteger y, BigInteger z, BigInteger w, BigPoint.BigPoint4D direction) {
-         float[] point = Container.manage().createArrayFloat(x.floatValue(), y.floatValue(), z.floatValue(), w.floatValue());
-         float[] secondPoint = direction.getBigPoint();
+     public BigVector4D(BigDecimal x, BigDecimal y, BigDecimal z, BigDecimal w, BigPoint.BigPoint4D direction) {
+         BigDecimal[] point = new Container<BigDecimal>().createArray(x, y, z, w);
+         BigDecimal[] secondPoint = direction.getBigPoint();
          bigVector4d = new BigVectorHelper4d() {
              @Override
-             public float[] getBigVec4() {
+             public BigDecimal[] getBigVec4() {
                  return point;
              }
 
              @Override
-             public float getDistanceBigVec4() {
+             public BigDecimal getDistanceBigVec4() {
                  return getDir(point, secondPoint);
              }
          };
      }
-        private float getDir(float[] point, float[] secondPoint) {
-            float start = 0;
+        private BigDecimal getDir(BigDecimal[] point, BigDecimal[] secondPoint) {
+            BigDecimal start = BigDecimal.ZERO;
             for(int i = 0; i < point.length; ++i) {
-                start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                start = start.add(point[i].abs().add(secondPoint[i].abs()));
             }
-            return (float)Math.sqrt(start);
+            return sqrt(start);
         }
-        public float[] getBigVector4d() {
+        public BigDecimal[] getBigVector4d() {
             return bigVector4d.getBigVec4();
         }
-        public float getDirection4d() {
+        public BigDecimal getDirection4d() {
             return bigVector4d.getDistanceBigVec4();
         }
     }
     public static class BigVectorInfiniteD {
         private static BigVectorHelperInfiniteD bigVectorID;
-        public BigVectorInfiniteD(BigInteger[] axis, BigPoint.BigPointInfiniteD direction) {
-            float[] point = new BigPoint.BigPointInfiniteD(axis).getBigPoint();
-            float[] secondPoint = direction.getBigPoint();
+        public BigVectorInfiniteD(BigDecimal[] axis, BigPoint.BigPointInfiniteD direction) {
+            BigDecimal[] point = new BigPoint.BigPointInfiniteD(axis).getBigPoint();
+            BigDecimal[] secondPoint = direction.getBigPoint();
             bigVectorID = new BigVectorHelperInfiniteD() {
                 @Override
-                public float[] getBigVecID() {
+                public BigDecimal[] getBigVecID() {
                  return point;
                 }
 
                 @Override
-                public float getDistanceBigVecID() {
+                public BigDecimal getDistanceBigVecID() {
                     return getDir(point, secondPoint);
                 }
             };
         }
-        private float getDir(float[] point, float[] secondPoint) {
-            float start = 0;
+        private BigDecimal getDir(BigDecimal[] point, BigDecimal[] secondPoint) {
+            BigDecimal start = BigDecimal.ZERO;
             for(int i = 0; i < point.length; ++i) {
-                start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                start = start.add(point[i].abs().add(secondPoint[i].abs()));
             }
-            return (float)Math.sqrt(start);
+            return sqrt(start);
         }
-        public float[] getBigVectorID() {
+        public BigDecimal[] getBigVectorID() {
             return bigVectorID.getBigVecID();
         }
-        public float getDirectionID() {
+        public BigDecimal getDirectionID() {
             return bigVectorID.getDistanceBigVecID();
         }
     }
     public static class DoubleBigVector {
      private static DoubleBigVectorHelper2d doubleBigVector2d;
      public DoubleBigVector(BigInteger x, BigInteger y, BigPoint.BigDoublePoint direction) {
-         double[] point = Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue());
-         double[] secondPoint = direction.getBigDoublePoint();
+         BigInteger[] point = new Container<BigInteger>().createArray(x, y);
+         BigInteger[] secondPoint = direction.getBigDoublePoint();
          doubleBigVector2d = new DoubleBigVectorHelper2d() {
              @Override
-             public double[] getDoubleBigVec2() {
-                 return Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue());
+             public BigInteger[] getDoubleBigVec2() {
+                 return new Container<BigInteger>().createArray(x, y);
              }
 
              @Override
-             public double getDistanceDoubleBigVec2() {
+             public BigInteger getDistanceDoubleBigVec2() {
                  return getDir(point, secondPoint);
              }
          };
      }
-        private double getDir(double[] point, double[] secondPoint) {
-            double start = 0.0;
+        private BigInteger getDir(BigInteger[] point, BigInteger[] secondPoint) {
+            BigInteger start = BigInteger.ZERO;
             for(int i = 0; i < point.length; ++i) {
-                start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                start = start.add(point[i].abs().add(secondPoint[i].abs()));
             }
-            return Math.sqrt(start);
+            return sqrt(new BigDecimal(start)).toBigInteger();
         }
-        public double[] getDoubleBigVector2d() {
+        public BigInteger[] getDoubleBigVector2d() {
             return doubleBigVector2d.getDoubleBigVec2();
         }
-        public double getDirection2d() {
+        public BigInteger getDirection2d() {
             return doubleBigVector2d.getDistanceDoubleBigVec2();
         }
         public static class DoubleBigVector3D {
          private static DoubleBigVectorHelper3d doubleBigVector3d;
          public DoubleBigVector3D(BigInteger x, BigInteger y, BigInteger z, BigPoint.BigDoublePoint.BigDoublePoint3D direction) {
-             double[] point = Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue(), z.doubleValue());
-             double[] secondPoint = direction.getBigDoublePoint();
+             BigInteger[] point = new Container<BigInteger>().createArray(x, y, z);
+             BigInteger[] secondPoint = direction.getBigDoublePoint();
              doubleBigVector3d = new DoubleBigVectorHelper3d() {
                  @Override
-                 public double[] getDoubleBigVec3() {
+                 public BigInteger[] getDoubleBigVec3() {
                      return point;
                  }
 
                  @Override
-                 public double getDistanceDoubleBigVec3() {
+                 public BigInteger getDistanceDoubleBigVec3() {
                      return getDir(point, secondPoint);
                  }
              };
          }
-            private double getDir(double[] point, double[] secondPoint) {
-                double start = 0.0;
+            private BigInteger getDir(BigInteger[] point, BigInteger[] secondPoint) {
+                BigInteger start = BigInteger.ZERO;
                 for(int i = 0; i < point.length; ++i) {
-                    start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                    start = start.add(point[i].abs().add(secondPoint[i].abs()));
                 }
-                return Math.sqrt(start);
+                return sqrt(new BigDecimal(start)).toBigInteger();
             }
-            public double[] getDoubleBigVector3d() {
+            public BigInteger[] getDoubleBigVector3d() {
              return doubleBigVector3d.getDoubleBigVec3();
             }
-            public double getDirection3d() {
+            public BigInteger getDirection3d() {
              return doubleBigVector3d.getDistanceDoubleBigVec3();
             }
         }
         public static class DoubleBigVector4D {
          private static DoubleBigVectorHelper4d doubleBigVector4d;
          public DoubleBigVector4D(BigInteger x, BigInteger y, BigInteger z, BigInteger w, BigPoint.BigDoublePoint.BigDoublePoint4D direction) {
-             double[] point = Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue(), z.doubleValue(), w.doubleValue());
-             double[] secondPoint = direction.getBigDoublePoint();
+             BigInteger[] point = new Container<BigInteger>().createArray(x, y, z, w);
+             BigInteger[] secondPoint = direction.getBigDoublePoint();
              doubleBigVector4d = new DoubleBigVectorHelper4d() {
                  @Override
-                 public double[] getDoubleBigVec4() {
+                 public BigInteger[] getDoubleBigVec4() {
                      return point;
                  }
 
                  @Override
-                 public double getDistanceDoubleBigVec4() {
+                 public BigInteger getDistanceDoubleBigVec4() {
                      return getDir(point, secondPoint);
                  }
              };
          }
-            private double getDir(double[] point, double[] secondPoint) {
-                double start = 0.0;
+            private BigInteger getDir(BigInteger[] point, BigInteger[] secondPoint) {
+                BigInteger start = BigInteger.ZERO;
                 for(int i = 0; i < point.length; ++i) {
-                    start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                    start = start.add(point[i].abs().add(secondPoint[i].abs()));
                 }
-                return Math.sqrt(start);
+                return sqrt(new BigDecimal(start)).toBigInteger();
             }
-            public double[] getDoubleBigVector4d() {
+            public BigInteger[] getDoubleBigVector4d() {
              return doubleBigVector4d.getDoubleBigVec4();
             }
-            public double getDirection4d() {
+            public BigInteger getDirection4d() {
              return doubleBigVector4d.getDistanceDoubleBigVec4();
             }
         }
         public static class DoubleBigVectorInfiniteD {
          private static DoubleBigVectorHelperInfiniteD doubleBigVectorID;
          public DoubleBigVectorInfiniteD(BigInteger[] axis, BigPoint.BigDoublePoint.BigDoublePointInfiniteD direction) {
-             double[] point = new BigPoint.BigDoublePoint.BigDoublePointInfiniteD(axis).getBigDoublePoint();
+             BigInteger[] point = new BigPoint.BigDoublePoint.BigDoublePointInfiniteD(axis).getBigDoublePoint();
              doubleBigVectorID = new DoubleBigVectorHelperInfiniteD() {
                  @Override
-                 public double[] getDoubleBigVecID() {
+                 public BigInteger[] getDoubleBigVecID() {
                      return point;
                  }
 
                  @Override
-                 public double getDistanceDoubleBigVecID() {
+                 public BigInteger getDistanceDoubleBigVecID() {
                      return getDir(point, direction.getBigDoublePoint());
                  }
              };
          }
-            private double getDir(double[] point, double[] secondPoint) {
-                double start = 0.0;
+            private BigInteger getDir(BigInteger[] point, BigInteger[] secondPoint) {
+                BigInteger start = BigInteger.ZERO;
                 for(int i = 0; i < point.length; ++i) {
-                    start = start + Math.abs(point[i]) + Math.abs(secondPoint[i]);
+                    start = start.add(point[i].abs().add(secondPoint[i].abs()));
                 }
-                return Math.sqrt(start);
+                return sqrt(new BigDecimal(start)).toBigInteger();
             }
-            public double[] getBigDoubleVectorID() {
+            public BigInteger[] getBigDoubleVectorID() {
              return doubleBigVectorID.getDoubleBigVecID();
             }
-            public double getDirectionID() {
+            public BigInteger getDirectionID() {
              return doubleBigVectorID.getDistanceDoubleBigVecID();
             }
         }
