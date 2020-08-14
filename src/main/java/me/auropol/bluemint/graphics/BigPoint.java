ee1bcb63be019596dd4c882d64a11d7fbbc8505e
Auropol <62548533+Auropol@users.noreply.github.com>
2020-08-14 18:34:33 +0200

    Restructurized BigPoint class
    Changed the support from primitive values with a 32 and 64 bit precision to BigInteger and BigDecimal values with 32bit max value pow 2 maximal precision

--- src/main/java/me/auropol/bluemint/graphics/BigPoint.java
+++ src/main/java/me/auropol/bluemint/graphics/BigPoint.java
@@ -2,147 +2,148 @@ package me.auropol.bluemint.graphics;
 
 import me.auropol.bluemint.primitive.Container;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class BigPoint {
     private static abstract class BigPointHelper2d {
-        public abstract float[] getBigPoint2d();
+        public abstract BigDecimal[] getBigPoint2d();
     }
     private static BigPointHelper2d point;
-    public BigPoint(BigInteger x, BigInteger y) {
+    public BigPoint(BigDecimal x, BigDecimal y) {
         point = new BigPointHelper2d() {
             @Override
-            public float[] getBigPoint2d() {
-                return Container.manage().createArrayFloat(x.floatValue(), y.floatValue());
+            public BigDecimal[] getBigPoint2d() {
+                return new Container<BigDecimal>().createArray(x, y);
             }
         };
     }
-    public float[] getBigPoint() {
+    public BigDecimal[] getBigPoint() {
         return point.getBigPoint2d();
     }
     private static abstract class BigPointHelper3d {
-        public abstract float[] getBigPoint3d();
+        public abstract BigDecimal[] getBigPoint3d();
     }
     public static class BigPoint3D {
         private static BigPointHelper3d point;
-        public BigPoint3D(BigInteger x, BigInteger y, BigInteger z) {
+        public BigPoint3D(BigDecimal x, BigDecimal y, BigDecimal z) {
             point = new BigPointHelper3d() {
                 @Override
-                public float[] getBigPoint3d() {
-                    return Container.manage().createArrayFloat(x.floatValue(), y.floatValue(), z.floatValue());
+                public BigDecimal[] getBigPoint3d() {
+                    return new Container<BigDecimal>().createArray(x, y, z);
                 }
             };
         }
-        public float[] getBigPoint() {
+        public BigDecimal[] getBigPoint() {
             return point.getBigPoint3d();
         }
     }
     private static abstract class BigPointHelper4d {
-        public abstract float[] getBigPoint4d();
+        public abstract BigDecimal[] getBigPoint4d();
     }
     public static class BigPoint4D {
         private static BigPointHelper4d point;
-        public BigPoint4D(BigInteger x, BigInteger y, BigInteger z, BigInteger w) {
+        public BigPoint4D(BigDecimal x, BigDecimal y, BigDecimal z, BigDecimal w) {
             point = new BigPointHelper4d() {
                 @Override
-                public float[] getBigPoint4d() {
-                    return Container.manage().createArrayFloat(x.floatValue(), y.floatValue(), z.floatValue(), w.floatValue());
+                public BigDecimal[] getBigPoint4d() {
+                    return new Container<BigDecimal>().createArray(x, y, z, w);
                 }
             };
         }
-        public float[] getBigPoint() {
+        public BigDecimal[] getBigPoint() {
             return point.getBigPoint4d();
         }
     }
     private static abstract class BigPointHelperInfiniteD {
-        public abstract float[] getBigPointID();
+        public abstract BigDecimal[] getBigPointID();
     }
     public static class BigPointInfiniteD {
         private static BigPointHelperInfiniteD point;
-        private static float[] f;
-        public BigPointInfiniteD(BigInteger[] axis) {
+        private static BigDecimal[] f;
+        public BigPointInfiniteD(BigDecimal[] axis) {
             point = new BigPointHelperInfiniteD() {
                 @Override
-                public float[] getBigPointID() {
-                    for(BigInteger bigInteger : axis) {
-                        f = Container.manage().createArrayFloat(bigInteger.floatValue());
+                public BigDecimal[] getBigPointID() {
+                    for(BigDecimal bigDec : axis) {
+                        f = new Container<BigDecimal>().createArray(bigDec);
                     }
                     return f;
                 }
             };
         }
-        public float[] getBigPoint() {
+        public BigDecimal[] getBigPoint() {
             return point.getBigPointID();
         }
     }
     public static class BigDoublePoint {
         private static abstract class BigDoublePointHelper2d {
-            public abstract double[] getBigPoint2d();
+            public abstract BigInteger[] getBigPoint2d();
         }
         private static BigDoublePointHelper2d point;
         public BigDoublePoint(BigInteger x, BigInteger y) {
             point = new BigDoublePointHelper2d() {
                 @Override
-                public double[] getBigPoint2d() {
-                    return Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue());
+                public BigInteger[] getBigPoint2d() {
+                    return new Container<BigInteger>().createArray(x, y);
                 }
             };
         }
-        public double[] getBigDoublePoint() {
+        public BigInteger[] getBigDoublePoint() {
             return point.getBigPoint2d();
         }
         private static abstract class BigDoublePointHelper3d {
-            public abstract double[] getBigPoint3d();
+            public abstract BigInteger[] getBigPoint3d();
         }
         public static class BigDoublePoint3D {
             private static BigDoublePointHelper3d point;
             public BigDoublePoint3D(BigInteger x, BigInteger y, BigInteger z) {
                 point = new BigDoublePointHelper3d() {
                     @Override
-                    public double[] getBigPoint3d() {
-                        return Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue(), z.doubleValue());
+                    public BigInteger[] getBigPoint3d() {
+                        return new Container<BigInteger>().createArray(x, y, z);
                     }
                 };
             }
-            public double[] getBigDoublePoint() {
+            public BigInteger[] getBigDoublePoint() {
                 return point.getBigPoint3d();
             }
         }
         private static abstract class BigDoublePointHelper4d {
-            public abstract double[] getBigPoint4d();
+            public abstract BigInteger[] getBigPoint4d();
         }
         public static class BigDoublePoint4D {
             private static BigDoublePointHelper4d point;
             public BigDoublePoint4D(BigInteger x, BigInteger y, BigInteger z, BigInteger w) {
                 point = new BigDoublePointHelper4d() {
                     @Override
-                    public double[] getBigPoint4d() {
-                        return Container.manage().createArrayDouble(x.doubleValue(), y.doubleValue(), z.doubleValue(), w.doubleValue());
+                    public BigInteger[] getBigPoint4d() {
+                        return new Container<BigInteger>().createArray(x, y, z, w);
                     }
                 };
             }
-            public double[] getBigDoublePoint()  {
+            public BigInteger[] getBigDoublePoint()  {
                 return point.getBigPoint4d();
             }
         }
         private static abstract class BigDoublePointHelperInfiniteD {
-            public abstract double[] getBigPointID();
+            public abstract BigInteger[] getBigPointID();
         }
         public static class BigDoublePointInfiniteD {
             private static BigDoublePointHelperInfiniteD point;
-            private static double[] d;
+            private static BigInteger[] d;
             public BigDoublePointInfiniteD(BigInteger[] axis) {
                 point = new BigDoublePointHelperInfiniteD() {
                     @Override
-                    public double[] getBigPointID() {
+                    public BigInteger[] getBigPointID() {
                         for(BigInteger bigInteger : axis) {
-                            d = Container.manage().createArrayDouble(bigInteger.doubleValue());
+                            d = new Container<BigInteger>().createArray(bigInteger);
                         }
                         return d;
                     }
                 };
             }
-            public double[] getBigDoublePoint() {return point.getBigPointID();}
+            public BigInteger[] getBigDoublePoint() {return point.getBigPointID();}
         }
     }
 }
